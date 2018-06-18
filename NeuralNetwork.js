f8f9ee3cafaf4f4da5754353253502f18435e129
ppenelon <penelon.pierre@gmail.com>
2018-06-18 20:35:11 +0200

    NeuralNetwork now have a specific js file

---
+++ NeuralNetwork.js
@@ -0,0 +1,187 @@
+class Neuron{
+
+    // weights = 0; // Poids de tous les neurones de la couche précédente vers ce neurone
+    // bias; // Bias de ce neurone
+
+    constructor(nbInputs){
+        // On initialise les poids avec une valeur entre -1 et 1
+        this.weights = [];
+        for(let i = 0; i < nbInputs; i++){
+            this.weights.push((Math.random() * 2) - 1);
+        }
+        // On initialise le bias avec une valeur entre -1 et 1
+        this.bias = (Math.random() * 2) - 1;
+    }
+
+    // Calcule la sortie du neurone
+    evaluate(inputs){
+        // Somme des valeurs d'entrées * les poids
+        let value = 0;
+        for(let i = 0; i < this.weights.length; i++){
+            value += inputs[i] * this.weights[i];
+        }
+        // On ajoute le bias
+        value += this.bias;
+        // On applique la fonction d'activation
+        return NeuralNetwork.sigmoid(value);
+    }
+}
+
+class NeuralNetwork{
+
+    // learningRate; // Taux d'apprentissage
+
+    // inputLayer; // Couche d'entrée
+    // hiddenLayer; // Couche cachée
+    // outputLayer; // Couche de sortie
+
+    constructor(nbInput, nbHidden, nbOutput, learningRate){
+        // Création des neurones de la couche d'entrée
+        this.inputLayer = [];
+        for(let i = 0; i < nbInput; i++){
+            this.inputLayer.push(new Neuron(0));
+        }
+        // Création des neurones de la couche cachée
+        this.hiddenLayer = [];
+        for(let i = 0; i < nbHidden; i++){
+            this.hiddenLayer.push(new Neuron(nbInput));
+        }
+        // Création des neurones de la couche de sortie
+        this.outputLayer = [];
+        for(let i = 0; i < nbOutput; i++){
+            this.outputLayer.push(new Neuron(nbHidden));
+        }
+        // Taux d'apprentissage
+        this.learningRate = learningRate;
+    }
+
+    // Evalue le resultat de valeur dans le réseau de neurones
+    evaluate(inputs){
+        // Calcul de la sortie des neurones de la couche cachée
+        let hiddenOutput = [];
+        for(let hiddenNeuron of this.hiddenLayer){
+            hiddenOutput.push(hiddenNeuron.evaluate(inputs));
+        }
+        // Calcul de la sortie des neurones de la couche de sortie
+        let outputOutput = [];
+        for(let outputNeuron of this.outputLayer){
+            outputOutput.push(outputNeuron.evaluate(hiddenOutput));
+        }
+        // On retourne la sortie des neurones de toutes les couches du réseau de neurones
+        return [
+            inputs,
+            hiddenOutput,
+            outputOutput
+        ];
+    }
+
+    learn(batchsInput, batchsOutput){
+        // On crée les tableaux qui vont recevoir les sommes de modification des poids et des bias
+        let weightsOutputHiddenModification = [];
+        let biasesOutputModification = [];
+        for(let i = 0; i < this.outputLayer.length; i++){ // i -> Index du neurone de sortie
+            // Poids
+            let outputNeuronToHiddenNeurons = [];
+            for(let j = 0; j < this.hiddenLayer.length; j++){ // j -> Index du neurone caché
+                outputNeuronToHiddenNeurons.push(0);
+            }
+            weightsOutputHiddenModification.push(outputNeuronToHiddenNeurons);
+            // Bias
+            biasesOutputModification.push(0);
+        }
+        let weightsHiddenInputModification = [];
+        let biasesHiddenModification = [];
+        for(let i = 0; i < this.hiddenLayer.length; i++){ // i -> Index du neurone caché
+            // Poids
+            let hiddenNeuronToInputNeurons = [];
+            for(let j = 0; j < this.inputLayer.length; j++){ // j -> Index du neurone d'entrée
+                hiddenNeuronToInputNeurons.push(0);
+            }
+            weightsHiddenInputModification.push(hiddenNeuronToInputNeurons);
+            // Bias
+            biasesHiddenModification.push(0);
+        }
+
+        // On parcours tous les batchs
+        for(let batchIndex = 0; batchIndex < batchsInput.length; batchIndex++){
+            // On récupère les entrées/sorties de ce batch
+            let inputs = batchsInput[batchIndex];
+            let outputs = batchsOutput[batchIndex];
+
+            // Calcul du résultat du réseau de neurones pour ces entrées
+            let neuralNetworkOutputs = this.evaluate(inputs);
+            let inputOutputs = neuralNetworkOutputs[0];
+            let hiddenOutputs = neuralNetworkOutputs[1];
+            let outputOutputs = neuralNetworkOutputs[2];
+            
+            // Gradients de la couche de sortie
+            let gradientsOutput = [];
+            for(let i = 0; i < this.outputLayer.length; i++){ // i -> Index du neurone de sortie
+                // Calcul de l'erreur au niveau du neurone de sortie
+                let error = outputs[i] - outputOutputs[i];
+                // Calcul de la dérivée de la fonction d'activation
+                let dsigmoidValue = NeuralNetwork.dsigmoid(outputOutputs[i]);
+                // Calcul du gradient
+                gradientsOutput.push(error * dsigmoidValue);
+            }
+
+            // Gradient de la couche cachée
+            let gradientsHidden = [];
+            for(let i = 0; i < this.hiddenLayer.length; i++){ // i -> Index du neurone cachée
+                // Calcul de l'erreur (somme de l'erreur des sorties * les poids associés entre le caché et la sortie)
+                let error = 0;
+                for(let j = 0; j < this.outputLayer.length; j++){ // j -> Index du neurone de sortie
+                    error += gradientsOutput[j] * this.outputLayer[j].weights[i];
+                }
+                // Calcul de la dérivée partielle de la valeur envoyée par ce neurone caché
+                let dsigmoidValue = NeuralNetwork.dsigmoid(hiddenOutputs[i]);
+                // Calcul du gradient
+                gradientsHidden.push(error * dsigmoidValue);
+            }
+
+            // Modification des poids et des bias des neurones de sortie
+            for(let i = 0; i < this.outputLayer.length; i++){ // i -> Index du neurone de sortie
+                // Poids
+                for(let j = 0; j < this.hiddenLayer.length; j++){ // j -> Index du neurone caché
+                    weightsOutputHiddenModification[i][j] += this.learningRate * gradientsOutput[i] * hiddenOutputs[j];
+                }
+                // Bias
+                biasesOutputModification[i] += this.learningRate * gradientsOutput[i];
+            }
+
+            // Modification des poids et des bias des neurones cachés
+            for(let i = 0; i < this.hiddenLayer.length; i++){ // i -> Index du neurone caché
+                // Poids
+                for(let j = 0; j < this.inputLayer.length; j++){ // j -> Index du neurone d'entrée
+                    weightsHiddenInputModification[i][j] += this.learningRate * gradientsHidden[i] * inputOutputs[j];
+                }
+                // Bias
+                biasesHiddenModification[i] += this.learningRate * gradientsHidden[i];
+            }
+        }
+
+        // On applique les modifications calculées aux poids et aux bias
+        for(let i = 0; i < this.outputLayer.length; i++){ // i -> Index du neurone de sortie
+            for(let j = 0; j < this.hiddenLayer.length; j++){ // j -> Index du neurone caché
+                this.outputLayer[i].weights[j] += weightsOutputHiddenModification[i][j];
+            }
+            this.outputLayer[i].bias += biasesOutputModification[i];
+        }
+        for(let i = 0; i < this.hiddenLayer.length; i++){ // i -> Index du neurone caché
+            for(let j = 0; j < this.inputLayer.length; j++){ // j -> Index du neurone d'entrée
+                this.hiddenLayer[i].weights[j] += weightsHiddenInputModification[i][j];
+            }
+            this.hiddenLayer[i].bias += biasesHiddenModification[i];
+        }
+    }
+
+    // Fonction d'activation
+    static sigmoid(x){
+        return 1 / (1 + Math.exp(-1 * x));
+    }
+
+    // Dérivée de la fonction d'activation (sachant que le paramètre y est une valeur déjà passée dans la fonction d'activation)
+    static dsigmoid(y){
+        return y * (1 - y);
+    }
+}
